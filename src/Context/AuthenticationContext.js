ee9ec3e7d1f5a3be48361f3292aaf6acc865598d
Jim <jameslotruglio@gmail.com>
2023-06-10 21:34:53 -0400

    set header to true

--- src/Context/AuthenticationContext.js
+++ src/Context/AuthenticationContext.js
@@ -47,7 +47,7 @@ export const AuthenticationProvider = ({children}) => {
                 withCredentials: true,                
                 headers: {
                     "Content-Type": "application/json",
-                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Origin": true,
                     "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
                     "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length"
                     
