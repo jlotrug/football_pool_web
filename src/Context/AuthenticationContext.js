352717901ce8b06a4a0117f25ae4a011798842dd
Jim <jameslotruglio@gmail.com>
2023-06-11 12:34:12 -0400

    headers

--- src/Context/AuthenticationContext.js
+++ src/Context/AuthenticationContext.js
@@ -20,8 +20,8 @@ export const AuthenticationProvider = ({children}) => {
         axios.post(url,userData
             , {
             headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
+            "Content-Type": 'application/x-www-form-urlencoded',
+            // "Access-Control-Allow-Origin": "*",
             // "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE",
             // "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length"
             
